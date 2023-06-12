82c40977870598d282c8d47063f9b7c059f567c5
Nathaly Goris Espino <nathalygorisespino@outlook.com>
2023-06-12 17:49:39 -0400

    Creando el menu

--- Models/CestaDeCompras.cs
+++ Models/CestaDeCompras.cs
@@ -5,4 +5,10 @@ public class CestaDeCompras
  public Cliente Cliente { get; set; } = new Cliente();
  public List<int> Ordenes { get; set; } = new List<int>();
  public bool HaPagado { get; set; }
+
+  public void Add(int pizzaId)
+ => Ordenes.Add(pizzaId);
+
+ public void RemoveAt(int pos)
+ => Ordenes.RemoveAt(pos);
  }
--- Models/Estado.cs
+++ Models/Estado.cs
@@ -1,8 +1,11 @@
  using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
- public class Estados
+ public class Estado
 {     
     public Menu Menu { get; } = new Menu();
     public CestaDeCompras Canasta { get; } = new CestaDeCompras();
     public UI UI { get; set; } = new UI();
+
+    public decimal PrecioTotal
+ => Canasta.Ordenes.Sum(id => Menu.GetPizza(id)!.Precio);
 }
